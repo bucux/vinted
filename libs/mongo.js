3508e5f8eab0498da43232534bb9536fd7353309
bucux <ubucux@gmail.com>
2024-03-10 16:54:25 +0100

    20240310 mongoUri

--- libs/mongo.js
+++ libs/mongo.js
@@ -4,8 +4,10 @@ let mongooseInstance = null;
 
 const mongoSingleton = () => {
   if (!mongooseInstance) {
+    const mongoURI = 'mongodb://127.0.0.1/vinteddb' // mongodb en local
+    if(process.env.HOSTNAME !== '17-pouces'){mongoURI = process.env.MONGO_ATLAS_URI}
     const mongoose = require('mongoose');
-    mongoose.connect('mongodb://127.0.0.1/vinteddb').then(() => {
+    mongoose.connect(mongoURI).then(() => {
       console.log('Connexion à MongoDB réussie.');
     }).catch((err) => {
       console.error('Erreur de connexion à MongoDB:', err);
